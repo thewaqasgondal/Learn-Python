28c16cfc36d9950891869318fe690230494d6aaf
Waqas Gondal <waqas@waqasg.com>
2018-07-16 23:50:48 +0500

    Function Added Commit

--- 01_Introduction/01_01_First.py
+++ 01_Introduction/01_01_First.py
@@ -5,6 +5,20 @@ print("Hello World!")
 a = 10
 b = 19
 
-r = a + b
 
-print(r)
+def sum(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print(sum(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
